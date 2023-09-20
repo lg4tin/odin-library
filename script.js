2c1503dad72ddc2e51b46078734200699b4b9d03
lg4tin <lg4tin@gmail.com>
2023-09-20 14:07:25 -0400

    Add modal and fix bugs with modal like allowing the cancel button to work with formmethod

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.toggle = function() {
-  if (this.read == 'Read') {
-    this.read = 'Not read';
-  } else {
-    this.read = 'Read'
+  if (this.read == 'read') {
+    this.read = 'not-read';
+  } else if (this.read == 'not-read') {
+    this.read = 'read'
   }
 }
 
@@ -35,26 +35,37 @@ function displayBooks() {
     //div.textContent = myLibrary[book].info();
     //create title
     let h3 = document.createElement('h3');
-    h3.textContent = myLibrary[book].title;
+    h3.textContent = `Title: ${myLibrary[book].title}`;
     div.appendChild(h3);
     //create author
     let author = document.createElement('p');
-    author.textContent = myLibrary[book].author;
+    author.textContent = `Author: ${myLibrary[book].author}`;
     div.appendChild(author);
     //create pages
     let pages = document.createElement('p');
-    pages.textContent = myLibrary[book].pages;
+    pages.textContent = `Pages: ${myLibrary[book].pages}`;
     div.appendChild(pages);
     // create read
     let read = document.createElement('p');
-    read.textContent = myLibrary[book].read;
+    read.textContent = `Status: ${myLibrary[book].read}`;
+    let toggleButton = document.createElement('button');
+    toggleButton.textContent = 'Change Status';
+    toggleButton.classList.add('toggle');
+    toggleButton.addEventListener('click', () => {
+      myLibrary[book].toggle();
+      read.textContent = `Status: ${myLibrary[book].read}`;
+      read.appendChild(toggleButton);
+    });
+    read.appendChild(toggleButton);
     div.appendChild(read);
     //create delete button
     let deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('delete');
     deleteButton.addEventListener('click', () => {
       div.remove();
       myLibrary.splice(book, 1);
+      displayBooks();
     })
     div.appendChild(deleteButton);
 
@@ -62,14 +73,21 @@ function displayBooks() {
   };
 }
 
-displayBooks();
+//displayBooks();
 
 const addBookButton = document.querySelector('.add-book');
 //addBookButton.addEventListener('click', displayBooks);
 addBookButton.addEventListener('click', addBook);
+addBookButton.addEventListener("click", (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  favDialog.close(selectEl.value); // Have to send the select box value here.
+});
 
 function addBook() {
-  if (title.value === '') return;
+  if (title.value === '' || author.value === '' || pages.value == '') {
+    alert('You must enter all info');
+    return;
+  };
   addBookToLibrary(new Book(title.value, author.value, pages.value, read.value))
   displayBooks();
   clearInputs();
@@ -82,3 +100,7 @@ function clearInputs() {
   read.value = '';
 }
 
+const showButton = document.getElementById("showDialog");
+showButton.addEventListener("click", () => {
+  favDialog.showModal();
+});
