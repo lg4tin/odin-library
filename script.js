feaf62c5dbcc66639000e98afb33b05bad2743d8
lg4tin <lg4tin@gmail.com>
2023-09-20 09:34:31 -0400

    Make it so that the books appear as cards and add delete button to the cards.

--- script.js
+++ script.js
@@ -14,14 +14,36 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
-  console.log(myLibrary);
 }
 
 function displayBooks() {
   container.innerHTML = '';
   for (let book in myLibrary) {
+    //create div card
     let div = document.createElement('div');
+    div.classList.add('card');
     div.textContent = myLibrary[book].info();
+    //create title
+    let h3 = document.createElement('h3');
+    h3.textContent = myLibrary[book].title;
+    div.appendChild(h3);
+    //create author
+    let author = document.createElement('p');
+    author.textContent = myLibrary[book].author;
+    div.appendChild(author);
+    //create pages
+    let pages = document.createElement('p');
+    pages.textContent = myLibrary[book].pages;
+    div.appendChild(pages);
+    //create delete button
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      div.remove();
+      myLibrary.splice(book, 1);
+    })
+    div.appendChild(deleteButton);
+
     container.appendChild(div);
   };
 }
