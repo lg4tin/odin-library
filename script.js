efc90afc62e8d93575647043d1ca36afe36e8a66
lg4tin <lg4tin@gmail.com>
2023-09-19 20:45:54 -0400

    Add starter functions and set up basic html

---
+++ script.js
@@ -0,0 +1,32 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function() {
+    return (`The ${title} by ${author}, ${pages} pages, ${read}`)
+  }
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
+
+const theHobbit = new Book('The Hobbit', 'Tolken', 500, 'I have not read');
+const harryPotter = new Book('Harry Potter', 'J.K. Rowling', 300, 'I have read');
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(harryPotter);
+
+function displayBooks() {
+  for (let book in myLibrary) {
+    console.log(myLibrary[book].info());
+  };
+}
+
+const addBookButton = document.querySelector('.add-book');
+addBookButton.addEventListener('click', displayBooks);
